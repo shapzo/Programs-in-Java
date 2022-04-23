e2c97574be6047d3c1a727aa62ce21d0199f52a7
shapzo <rodrigopalma98@outlook.com>
2022-04-23 15:22:50 -0500

    Medical prescription

--- Medical prescription/consulta.java
+++ Medical prescription/consulta.java
@@ -1,21 +1,10 @@
-import java.awt.Checkbox;
-import java.awt.Color;
-import java.awt.Container;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.*;
+import java.awt.event.*;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
-import javax.swing.ButtonGroup;
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
-import javax.swing.JComboBox;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JRadioButton;
-import javax.swing.JTextField;
+import javax.swing.*;
 
 public class consulta extends JFrame {
 
