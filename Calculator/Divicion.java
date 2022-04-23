5600ddead7dea67ac57e85997b99603611d8ffb0
shapzo <rodrigopalma98@outlook.com>
2022-04-23 10:43:11 -0500

    Calculator

--- Calculator/Divicion.java
+++ Calculator/Divicion.java
@@ -17,31 +17,30 @@ public class Divicion extends JFrame {
 	JTextField campo1;
 	JTextField campo2;
 
-	JLabel fondo;
-
 	public JPanel p = new JPanel();
 
 	public Divicion() {
 
-		setTitle("Suma");
+		setTitle("Divicion");
 		setSize(600, 600);
+		setLocationRelativeTo(null);
 		p.setBackground(Color.black);
 		p.setLayout(null);
 		Container zona = getContentPane();
 
 		zona.add(p);
 
-		etiqueta = new JLabel("Ingrese el 1� numero: ");
-		etiqueta1 = new JLabel("Ingrese el 2� numero: ");
-		etiqueta2 = new JLabel("Resultado: ");
+		etiqueta = new JLabel("1st number: ");
+		etiqueta1 = new JLabel("2nd number: ");
+		etiqueta2 = new JLabel("Result: ");
 
 		campo = new JTextField(" ");
 		campo1 = new JTextField(" ");
 		campo2 = new JTextField(" ");
 
-		boton = new JButton("Dividir");
-		boton1 = new JButton("Limpiar");
-		boton2 = new JButton(new ImageIcon("flecha.png"));
+		boton = new JButton("Split");
+		boton1 = new JButton("Clean");
+		boton2 = new JButton(new ImageIcon("Images/exit.png"));
 
 		campo.setBorder(new javax.swing.border.LineBorder(Color.yellow, 3, true));
 		campo.setBackground(Color.blue);
@@ -50,9 +49,9 @@ public class Divicion extends JFrame {
 		campo2.setBorder(new javax.swing.border.LineBorder(Color.red, 3, true));
 		campo2.setBackground(Color.green);
 
-		campo.setFont(new java.awt.Font("Comic Sans MS", 2, 20));
-		campo1.setFont(new java.awt.Font("Comic Sans MS", 2, 20));
-		campo2.setFont(new java.awt.Font("Comic Sans MS", 2, 20));
+		campo.setFont(new java.awt.Font("mononoki Nerd Font", 1, 20));
+		campo1.setFont(new java.awt.Font("mononoki Nerd Font", 1, 20));
+		campo2.setFont(new java.awt.Font("mononoki Nerd Font", 1, 20));
 
 		campo.setForeground(Color.pink);
 		campo1.setForeground(Color.pink);
@@ -62,29 +61,24 @@ public class Divicion extends JFrame {
 		etiqueta1.setForeground(Color.orange);
 		etiqueta2.setForeground(Color.orange);
 
-		etiqueta.setFont(new java.awt.Font("Comic Sans MS", 2, 20));
-		etiqueta1.setFont(new java.awt.Font("Comic Sans MS", 2, 20));
-		etiqueta2.setFont(new java.awt.Font("Comic Sans MS", 2, 20));
+		etiqueta.setFont(new java.awt.Font("comfortaa", 1, 20));
+		etiqueta1.setFont(new java.awt.Font("comfortaa", 1, 20));
+		etiqueta2.setFont(new java.awt.Font("comfortaa", 1, 20));
 
-		boton.setFont(new java.awt.Font("Consolas", 2, 20));
-		boton1.setFont(new java.awt.Font("Consolas", 2, 20));
-		boton2.setFont(new java.awt.Font("Consolas", 2, 20));
+		boton.setFont(new java.awt.Font("comfortaa", 2, 20));
+		boton1.setFont(new java.awt.Font("comfortaa", 2, 20));
 
 		etiqueta.setBounds(25, 50, 180, 30);
 		etiqueta1.setBounds(25, 130, 180, 30);
 		etiqueta2.setBounds(25, 300, 180, 30);
 
-		campo.setBounds(190, 50, 150, 30);
-		campo1.setBounds(190, 130, 150, 30);
-		campo2.setBounds(185, 300, 150, 30);
+		campo.setBounds(210, 50, 150, 30);
+		campo1.setBounds(210, 130, 150, 30);
+		campo2.setBounds(210, 300, 150, 30);
 
-		boton.setBounds(185, 200, 150, 50);
-		boton1.setBounds(350, 295, 150, 35);
-		boton2.setBounds(185, 400, 150, 50);
-
-		fondo = new JLabel();
-		fondo.setIcon(new ImageIcon("image1.jpg"));
-		fondo.setBounds(1, 1, 600, 600);
+		boton.setBounds(210, 200, 150, 50);
+		boton1.setBounds(370, 295, 150, 35);
+		boton2.setBounds(210, 400, 150, 50);
 
 		boton.setBackground(new java.awt.Color(112, 24, 224));
 		boton1.setBackground(new java.awt.Color(24, 163, 224));
@@ -100,7 +94,6 @@ public class Divicion extends JFrame {
 		p.add(boton);
 		p.add(boton1);
 		p.add(boton2);
-		p.add(fondo);
 
 		boton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent v) {
@@ -111,9 +104,9 @@ public class Divicion extends JFrame {
 				double x1 = Double.parseDouble(cad1);
 				double x2 = Double.parseDouble(cad2);
 
-				double suma = x1 / x2;
+				double Div = x1 / x2;
 
-				campo2.setText("= " + suma);
+				campo2.setText("= " + Div);
 
 			}
 		});
@@ -130,10 +123,8 @@ public class Divicion extends JFrame {
 		boton2.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent evt) {
 				dispose();
-
 			}
 		});
-
 	}
 
 	public static void main(String... RODRIGO) {
